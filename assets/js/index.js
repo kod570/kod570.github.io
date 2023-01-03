db4c04ac9aa5b8aa45948419cad4595e163b5902
kod570 <60658985+kod570@users.noreply.github.com>
2023-01-03 10:39:53 +0100

    update

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// Array med alle knapper
+
+
+// FOR loop som looper igennem alle klapper i den Array
+// - Ind ei for loop skal vi tilføje et klik event på alle knapper. Udskiv "event" i console.log
+
+
+let faqs = document.querySelectorAll(".faq-item header button");
+let i;
+
+for (let i = 0; i < faqs.length; i++) {
+    faqs[i].addEventListener("click", function() {
+        this.parentNode.parentNode.classList.toggle("active");
+
+      });
+}
